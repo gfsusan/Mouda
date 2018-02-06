661a61dbb3b72b058a180cddfcdfb7a4b2b12026
Susan Kim <gfsusan@naver.com>
2018-02-06 14:33:39 +0900

    Modified layout of Add Tab

---
+++ moudaTab2/AddVC.swift
@@ -0,0 +1,37 @@
+//
+//  AddVC.swift
+//  moudaTab2
+//
+//  Created by cauadc on 2018. 2. 6..
+//  Copyright © 2018년 cauadc. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    @IBOutlet weak var lineTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
