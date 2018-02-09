613bd7fe7d4ff832aa82e5ed51cbf062ea2da9fb
isabellajoo <motoma_214@naver.com>
2018-02-09 22:15:46 +0900

    modified AppLockTableVC

--- moudaTab2/Setting/AppLockTableVC.swift
+++ moudaTab2/Setting/AppLockTableVC.swift
@@ -11,17 +11,22 @@ import UIKit
 class AppLockTableVC: UITableViewController {
 
     
+    @IBOutlet weak var appLockSwitch: UISwitch!
+    @IBOutlet weak var touchIDSwitch: UISwitch!
+    @IBOutlet weak var touchIDLabel: UILabel!
+    @IBOutlet weak var changePasswordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-
+    
+    @IBAction func appLockStatus(_ sender: Any) {
+    }
+    
+    @IBAction func touchIDStatus(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
