b4ac6ed8ce39be80187b7d7d263d17ac4d37aaf6
Susan Kim <gfsusan@naver.com>
2018-02-10 18:15:38 +0900

    Able to delete Feeds

--- moudaTab2/MyFeedTab/FeedDetailVC.swift
+++ moudaTab2/MyFeedTab/FeedDetailVC.swift
@@ -9,14 +9,35 @@
 import UIKit
 
 class FeedDetailVC: UIViewController {
+    
+    var feed:Feed?
+    let formatDate = DateFormatter()
+    var indexPath:Int?
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var lineLabel: UILabel!
     @IBOutlet weak var pageLabel: UILabel!
     @IBOutlet weak var thoughtLabel: UILabel!
     
-    var feed:Feed?
-    let formatDate = DateFormatter()
+    
+    @IBAction func deletePressed(_ sender: Any) {
+        let alertView = UIAlertController(title: "삭제", message: "정말로 피드를 삭제하시겠습니까?", preferredStyle: .alert)
+        let delete = UIAlertAction(title: "피드 삭제", style: .destructive) { (action) in
+            if let index = self.indexPath {
+                dataCenter.delete(feedAt: index)
+                self.navigationController?.popToRootViewController(animated: true)
+            } else {
+                dataCenter.createAlert(title: "삭제에 실패했습니다.", message: "다시 시도해 보세요.", sender: self)
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .default) {(action) in
+        }
+        alertView.addAction(cancel)
+        alertView.addAction(delete)
+        present(alertView, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
