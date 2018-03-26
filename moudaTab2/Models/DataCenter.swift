586b141b95d8da03e6a125aa3ca87b14c16a9f73
Susan Kim <gfsusan@naver.com>
2018-03-26 17:08:33 +0900

    Edited dummy data

--- moudaTab2/Models/DataCenter.swift
+++ moudaTab2/Models/DataCenter.swift
@@ -46,10 +46,10 @@ class DataCenter {
             print("피드 파일 있음")
             feeds += unarchArray
 
-
         } else {
             print("피드 파일 없음")
-            feeds += defaultFeedData()
+//            feeds += defaultFeedData()
+            feeds += [Feed(book: Book(title: "모으다에 오신 걸 환영합니다.", coverImageURL: "http://cfile5.uf.tistory.com/image/214D5C37583EE5952C31D0", publisher: "모으다", writer: "모으다개발자", bookDescription: "nothing"), page: 100, line: "'쓰다' 버튼을 터치해 간직하고 싶은 책 속의 한 문장을 작성해주세요.", thought: "기록한 문장에 대한 본인만의 생각이나 감정을 표현해보세요.")]
         }
 
 
@@ -58,12 +58,14 @@ class DataCenter {
             bookmarks += unarchArray
         } else {
             print("북마크 파일 없음")
-            bookmarks += defaultBookmarkData()
+//            bookmarks += defaultBookmarkData()
         }
 
         // dummy data
 //        feeds += defaultFeedData()
 //        bookmarks += defaultBookmarkData()
+        
+//        feeds += [Feed(book: Book(title: "모으다에 오신 걸 환영합니다.", coverImageURL: "http://cfile5.uf.tistory.com/image/214D5C37583EE5952C31D0", publisher: "모으다", writer: "모으다개발자", bookDescription: "nothing"), page: 100, line: "'쓰다' 버튼을 터치해 간직하고 싶은 책 속의 한 문장을 작성해주세요.", thought: "기록한 문장에 대한 본인만의 생각이나 감정을 표현해보세요.")]
         
 //        for feed in feeds {
 //            books.append(feed.book)
