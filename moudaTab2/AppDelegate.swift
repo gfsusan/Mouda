71d06052e5dd0afd06983a12c815378790acf6d4
isabellajoo <motoma_214@naver.com>
2018-04-09 18:36:05 +0900

    update passcode

--- moudaTab2/AppDelegate.swift
+++ moudaTab2/AppDelegate.swift
@@ -30,10 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        //Keychain
-        sleep(1)
-        LoginCheck.set(1, forKey: "LoginAvailable")
-        
         print("App Delegate")
         // 네비게이션 바 색상
         UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 82/255, green: 62/255, blue: 53/255, alpha: 1)
---
+++ moudaTab2/Setting/Passcode/Source/UI.swift
@@ -0,0 +1,53 @@
+//
+//  UI.swift
+//  moudaTab2
+//
+//  Created by MBP04 on 2018. 4. 9..
+//  Copyright © 2018년 cauadc. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func rounded() {
+        layer.cornerRadius = frame.width/2
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func shake(delegate: CAAnimationDelegate) {
+        let animationKeyPath = "transform.translation.x"
+        let shakeAnimation = "shake"
+        let duration = 0.6
+        let animation = CAKeyframeAnimation(keyPath: animationKeyPath)
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = duration
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0]
+        animation.delegate = delegate
+        layer.add(animation, forKey: shakeAnimation)
+    }
+}
+
+class Indicator: UIView {
+    
+    var isNeedClear = false
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        rounded()
+    }
+}
+
+class ImageView: UIImageView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        rounded()
+    }
+}
+
+class Button: UIButton {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        rounded()
+    }
+}
