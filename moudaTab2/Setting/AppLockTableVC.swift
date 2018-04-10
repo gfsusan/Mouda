8841a1e25a08986d08d7bfd2b11921abd4d1bc23
isabellajoo <motoma_214@naver.com>
2018-04-10 22:53:43 +0900

    temp

--- moudaTab2/Setting/AppLockTableVC.swift
+++ moudaTab2/Setting/AppLockTableVC.swift
@@ -17,10 +17,20 @@ class AppLockTableVC: UITableViewController {
     @IBOutlet weak var changePasswordLabel: UILabel!
     
     @IBAction func appLock(_ sender: UISwitch) {
-                
+        if appLockSwitch.isOn == true {
+            LoginCheck.set(1, forKey: "LoginAvailable")
+            pin(.create)
+            PWDatas.login.state = true
+        } else if appLockSwitch.isOn == false {
+            LoginCheck.set(0, forKey: "LoginAvailable")
+            pin(.deactive)
+            PWDatas.login.state = false
+        }
+        PWDatas.save()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        appLockSwitch.isOn = PWDatas.login.state
         
     }
     
@@ -28,6 +38,15 @@ class AppLockTableVC: UITableViewController {
     }
     
     @IBAction func touchIDStatus(_ sender: Any) {
+    }
+    
+    func pin(_ mode: ALMode) {
+        
+        var appearance = ALAppearance()
+        appearance.title = "비밀번호를 입력해주세요."
+        appearance.isSensorsEnabled = true
+        
+        AppLocker.present(with: mode, and: appearance)
     }
     
     override func didReceiveMemoryWarning() {
