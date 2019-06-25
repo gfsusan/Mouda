43605141506c8799b7193f12779e9e1fd90699f6
Susan <suzenyada@gmail.com>
2019-06-25 11:12:36 +0900

    Removed keychain related files

--- Mouda/AppDelegate.swift
+++ Mouda/AppDelegate.swift
@@ -27,12 +27,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        //Keychain
-        sleep(1)
-        LoginCheck.set(1, forKey: "LoginAvailable")
         
         print("App Delegate")
         // 네비게이션 바 색상
