47527ab2c2aca6f3e3234628f2c5d41cf49f8983
Susan <suzenyada@gmail.com>
2019-06-25 11:29:53 +0900

    Added template view controller file

---
+++ Mouda/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mouda
+//
+//  Created by Susan on 25/06/2019.
+//  Copyright © 2019 cauadc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureConstraints()
+        addTargets()
+    }
+    
+    func configureConstraints() {
+        
+    }
+    
+    func addTargets() {
+        
+    }
+}
