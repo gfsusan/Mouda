3fb8d3ef09ba41beeb4bc81c893cd622d200488c
Susan Kim <suzenyada@gmail.com>
2019-07-01 04:48:09 +0900

    Removed unused funcs

--- Mouda/Extensions/UIFont+Extension.swift
+++ Mouda/Extensions/UIFont+Extension.swift
@@ -9,67 +9,13 @@
 import Foundation
 import UIKit
 
-struct AppFontName {
-    static let regular = "NanumMyeongjoOTF"
-    static let extraBold = "NanumMyeongjoOTFExtraBold"
-}
-
-extension UIFontDescriptor.AttributeName {
-    static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
-}
-
 extension UIFont {
     
     @objc class func mySystemFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: AppFontName.regular, size: size) ?? .systemFont(ofSize: size)
+        return UIFont(name: "NanumMyeongjoOTF", size: size) ?? .systemFont(ofSize: size)
     }
     
     @objc class func myExtraBoldSystemFont(ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: AppFontName.extraBold, size: size) ?? .systemFont(ofSize: size)
-    }
-    
-    @objc convenience init(myCoder aDecoder: NSCoder) {
-        guard
-            let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor,
-            let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String else {
-                self.init(myCoder: aDecoder)
-                return
-        }
-        var fontName = ""
-        switch fontAttribute {
-        case "CTFontRegularUsage":
-            fontName = AppFontName.regular
-        case "CTFontEmphasizedUsage", "CTFontBoldUsage":
-            fontName = AppFontName.extraBold
-        case "CTFontObliqueUsage":
-            fontName = AppFontName.extraBold
-        default:
-            fontName = AppFontName.regular
-        }
-        self.init(name: fontName, size: fontDescriptor.pointSize)!
-    }
-    
-    class func overrideInitialize() {
-        guard self == UIFont.self else { return }
-        
-        if let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:))),
-            let mySystemFontMethod = class_getClassMethod(self, #selector(mySystemFont(ofSize:))) {
-            method_exchangeImplementations(systemFontMethod, mySystemFontMethod)
-        }
-        
-        if let boldSystemFontMethod = class_getClassMethod(self, #selector(boldSystemFont(ofSize:))),
-            let myBoldSystemFontMethod = class_getClassMethod(self, #selector(myExtraBoldSystemFont(ofSize:))) {
-            method_exchangeImplementations(boldSystemFontMethod, myBoldSystemFontMethod)
-        }
-        
-        if let italicSystemFontMethod = class_getClassMethod(self, #selector(italicSystemFont(ofSize:))),
-            let myItalicSystemFontMethod = class_getClassMethod(self, #selector(myExtraBoldSystemFont(ofSize:))) {
-            method_exchangeImplementations(italicSystemFontMethod, myItalicSystemFontMethod)
-        }
-        
-        if let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:))), // Trick to get over the lack of UIFont.init(coder:))
-            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:))) {
-            method_exchangeImplementations(initCoderMethod, myInitCoderMethod)
-        }
+        return UIFont(name: "NanumMyeongjoOTFExtraBold", size: size) ?? .systemFont(ofSize: size)
     }
 }
