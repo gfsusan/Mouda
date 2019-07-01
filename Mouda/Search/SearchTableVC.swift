a72b28f7de12fb038a85901396d48d74cc3aa67e
Susan Kim <suzenyada@gmail.com>
2019-07-01 13:48:38 +0900

    Refactor search controller

--- Mouda/Search/SearchTableVC.swift
+++ Mouda/Search/SearchTableVC.swift
@@ -11,38 +11,47 @@ import LBTATools
 
 private let cellId = "bookInfoCellId"
 
-class SearchTableVC: UITableViewController,  UISearchBarDelegate, XMLParserDelegate {
+class SearchTableVC: UITableViewController, UISearchBarDelegate, UISearchResultsUpdating, XMLParserDelegate {
     
-    var books:[Book] = []
-    var addPopUpDelegate:AddFeedVC?
-    var addBookmarkDelegate:AddBookmarkVC?
+    var searchResult: [Book] = []
+    var addPopUpDelegate: AddFeedVC?
+    var addBookmarkDelegate: AddBookmarkVC?
     
-    let searchBar: UISearchBar = {
-        let sb = UISearchBar()
-        return sb
+    lazy var searchController: UISearchController = {
+        let sc = UISearchController(searchResultsController: nil)
+        sc.searchBar.barStyle = .blackTranslucent
+        sc.searchBar.tintColor = .white
+        return sc
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureConstraints()
-        addTargets()
+        configureSearchBar()
         
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "검색"
+
         tableView.register(SearchBookCell.self, forCellReuseIdentifier: cellId)
-        
-        searchBar.delegate = self
-        books = dataCenter.books
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
     }
     
-    func configureConstraints() {
-        tableView.tableHeaderView = searchBar
-        searchBar.withHeight(44)
-        searchBar.widthAnchor.constraint(equalTo: tableView.widthAnchor).isActive = true
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.3) {
+            self.navigationItem.hidesSearchBarWhenScrolling = false
+        }
+
+        navigationItem.hidesSearchBarWhenScrolling = true
     }
     
-    func addTargets() {
-        
+    func configureSearchBar() {
+        searchController.searchResultsUpdater = self
+        self.navigationItem.searchController = searchController
     }
     
     // ClientID
@@ -51,56 +60,6 @@ class SearchTableVC: UITableViewController,  UISearchBarDelegate, XMLParserDeleg
     // a_I3ZGoXXY
     
     // https://openapi.naver.com/v1/search/book.xml
-    
-    // searchBar 검색 버튼 클릭
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        books = []
-        if (searchBar.text == "") {
-            return
-        }
-        
-        let urlString = "https://openapi.naver.com/v1/search/book.xml?query=" + searchBar.text!
-        let urlWithPercentEscapes = urlString.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)
-        let url = URL(string: urlWithPercentEscapes!)
-        
-        var request = URLRequest(url: url!)
-        request.addValue("application/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
-        request.addValue("svT_lXMu_njLXKNKBA2X", forHTTPHeaderField: "X-Naver-Client-Id")
-        request.addValue("a_I3ZGoXXY", forHTTPHeaderField: "X-Naver-Client-Secret")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard error == nil else {
-                print(error as Any)
-                return
-            }
-            guard let data = data else {
-                print("Data is empty")
-                return
-            }
-            
-            // 수신한 데이터 출력
-            print(String(data: data, encoding: String.Encoding.utf8) as Any)
-            
-            self.item?.title = ""
-            self.item?.coverImageURL = ""
-            self.item?.publisher = ""
-            self.item?.writer = ""
-            
-            // Parse the XML
-            let parser = XMLParser(data: Data(data))
-            parser.delegate = self
-            let success:Bool = parser.parse()
-            if success {
-                print("parse success!")
-                print(self.strXMLData as Any)
-            } else {
-                print("parse failure!")
-            }
-        }
-        
-        task.resume()
-        
-    }
     
     // MARK: - XML delegate
     var currentElement : String? = ""
@@ -130,54 +89,37 @@ class SearchTableVC: UITableViewController,  UISearchBarDelegate, XMLParserDeleg
             item?.publisher = temp.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
         } else if elementName == "author" {
             item?.writer = temp.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
-            books.append(self.item!)
+            searchResult.append(self.item!)
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
-        
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         temp += string
     }
     
-    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return books.count
+        return searchResult.count
     }
 
-   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SearchBookCell
-
+        let errCell = UITableViewCell()
         
-        let book = books[indexPath.row]
-        // Configure the cell...
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? SearchBookCell else { return errCell }
+        guard indexPath.row < searchResult.count else { return cell }
         
-        if let thumbImage = book.coverImage {
-            cell.bookImageView.image = thumbImage
-        } else {
-            cell.bookImageView.image = UIImage(named: "book2")
-            if let thumbImageURL = book.coverImageURL {
-                DispatchQueue.main.async(execute: {
-                    book.coverImage = book.getCoverImage(withURL: thumbImageURL)
-                    guard let thumbImage = book.coverImage else {
-                        return
-                    }
-                    cell.bookImageView.image = thumbImage
-                })
-            }
-            
-            
+        let book = searchResult[indexPath.row]
+        
+        if let thumbImageURL = book.coverImageURL {
+            cell.bookImageView.loadImageUsingUrlString(imageUrl: thumbImageURL)
         }
         cell.titleTV.text = book.title
         cell.writerTV.text = book.writer
@@ -191,12 +133,60 @@ class SearchTableVC: UITableViewController,  UISearchBarDelegate, XMLParserDeleg
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        self.addPopUpDelegate?.book = books[indexPath.row]
+        self.addPopUpDelegate?.book = searchResult[indexPath.row]
         
-        self.addBookmarkDelegate?.book = books[indexPath.row]
+        self.addBookmarkDelegate?.book = searchResult[indexPath.row]
 
         navigationController?.popViewController(animated: true)
-        
     }
     
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else { return }
+        guard searchText != "" else { return }
+        
+        searchResult = []
+        
+        let urlString = "https://openapi.naver.com/v1/search/book.xml?query=" + searchText
+        guard let urlWithPercentEscapes = urlString.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) else { return }
+        guard let url = URL(string: urlWithPercentEscapes) else { return }
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.addValue("svT_lXMu_njLXKNKBA2X", forHTTPHeaderField: "X-Naver-Client-Id")
+        request.addValue("a_I3ZGoXXY", forHTTPHeaderField: "X-Naver-Client-Secret")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print(error as Any)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            // 수신한 데이터 출력
+            print(String(data: data, encoding: String.Encoding.utf8) as Any)
+            
+            self.item?.title = ""
+            self.item?.coverImageURL = ""
+            self.item?.publisher = ""
+            self.item?.writer = ""
+            
+            // Parse the XML
+            let parser = XMLParser(data: Data(data))
+            parser.delegate = self
+            let success:Bool = parser.parse()
+            if success {
+                debugPrint("parse success!")
+                debugPrint(self.strXMLData as Any)
+            } else {
+                debugPrint("parse failure!")
+            }
+        }
+        
+        task.resume()
+    }
 }
