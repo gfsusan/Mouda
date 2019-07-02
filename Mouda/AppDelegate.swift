e0d85828c7b98b14890a1b422a37248fe74ec1d2
Susan Kim <suzenyada@gmail.com>
2019-07-03 02:01:59 +0900

    Refactor AppDelegate.swift

--- Mouda/AppDelegate.swift
+++ Mouda/AppDelegate.swift
@@ -1,16 +1,3 @@
-/*
- TODO
- 1. iCloud에 어떻게 저장하는 지 알아보기
- 2. 설정 탭 내용
- 3. 북마크 테이블 뷰
- 4. 책 검색
- 5. 아이콘 화질 ---> 코딘 ??????? :)
-
- 하면 좋은것
- 1. AddVC 모달 띄우기
- 
- */
-
 //
 //  AppDelegate.swift
 //  Mouda
@@ -28,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         // 네비게이션 바 색상
         UINavigationBar.appearance().barTintColor = UIColor(displayP3Red: 82/255, green: 62/255, blue: 53/255, alpha: 1)
@@ -50,34 +36,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let tabBarController = TabBarController()
         tabBarController.viewControllers = [myFeedNavContr, addNavContr, bookmarkNavContr]
         tabBarController.tabBar.tintColor = .tabBarTintColor
+        
         window = UIWindow()
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
         
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         dataCenter.save()
     }
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         dataCenter.save()
     }
 
